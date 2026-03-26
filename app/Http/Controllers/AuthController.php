4affacf9748033495487945be903871cd9fdf6fd
Richhacker45 <117771605+RICHHACKER45@users.noreply.github.com>
2026-03-26 14:49:18 +0800

    fix: added DB facades library

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
