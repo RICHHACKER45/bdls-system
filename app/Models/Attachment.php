985ebb2748755677520cb6005a0ed0de65ebc95a
Richhacker45 <117771605+RICHHACKER45@users.noreply.github.com>
2026-03-31 14:18:36 +0800

    added fillable protected

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'service_request_id',
+        'file_path',
+    ];
 }
