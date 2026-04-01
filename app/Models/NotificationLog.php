1d9e82b98beb52da4e2b53636045a9e404a0e64b
Richhacker45 <117771605+RICHHACKER45@users.noreply.github.com>
2026-04-01 13:30:42 +0800

    added a fillable mass aassignment protection

--- app/Models/NotificationLog.php
+++ app/Models/NotificationLog.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotificationLog extends Model
 {
-    //
+    use HasFactory;
+
+    // THE LARAVEL WAY: Mass Assignment Protection
+    protected $fillable = [
+        'user_id',
+        'service_request_id',
+        'channel',
+        'recipient_contact',
+        'message_content',
+        'status',
+        'provider_response',
+    ];
 }
