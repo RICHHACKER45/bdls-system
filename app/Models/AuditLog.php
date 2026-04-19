cc6dcd70698f98659bf26453ec3b5ab5a4b851aa
Richhacker45 <117771605+RICHHACKER45@users.noreply.github.com>
2026-04-19 22:43:50 +0800

    chore:added a mass assignment protection

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuditLog extends Model
 {
-    //
+    // 1. THE LARAVEL WAY: I-disable ang updated_at dahil bawal i-edit ang log
+    const UPDATED_AT = null;
+
+    // 2. SECURITY: Mass Assignment Protection
+    protected $fillable = [
+        'admin_id',
+        'action',
+        'description',
+    ];
+
+    // 3. ELOQUENT RELATIONSHIP: Kunin ang data ng Admin na gumawa ng action
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
 }
